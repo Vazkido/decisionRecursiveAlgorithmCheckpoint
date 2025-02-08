e80a6aff5f75f3db01a4b9eeada94bb4e2f0e1e4
vazkido <vazkid02@gmail.com>
2025-02-08 12:03:35 -0500

    fibonacci update

--- Recursion/fibonacci.js
+++ Recursion/fibonacci.js
@@ -1,12 +1,26 @@
 function fibonacciChecker(n) {
-  if (n <= 1) {
-    return n;
-  } else {
-    return fibonacciChecker(n - 1) + fibonacciChecker(n + 2);
-  }
+  if (n <= 0) return 0;
+  if (n <= 1) return 1;
+  return fibonacciChecker(n - 1) + fibonacciChecker(n - 2);
 }
 
-//let try out an example
-const n = prompt("Enter the postion of the Fibonacci sequence:");
-const result = fibonacciChecker(parseInt(n));
-console.log(`The ${n}th Fibonacci number is ${result}.`);
+//Example usage:
+console.log(fibonacciChecker(6));
+console.log(fibonacciChecker(10));
+
+
+
+
+
+// function fibonacciChecker(n) {
+//   if (n <= 1) {
+//     return n;
+//   } else {
+//     return fibonacciChecker(n - 1) + fibonacciChecker(n + 2);
+//   }
+// }
+
+// //let try out an example
+// const n = prompt("Enter the postion of the Fibonacci sequence:");
+// const result = fibonacciChecker(parseInt(n));
+// console.log(`The ${n}th Fibonacci number is ${result}.`);
