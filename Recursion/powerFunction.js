0c36a71e767ae1293acb6f0531d58c0c5b3a0732
vazkido <vazkid02@gmail.com>
2025-02-10 16:27:37 -0500

    JS update power 1.1

--- Recursion/powerFunction.js
+++ Recursion/powerFunction.js
@@ -2,3 +2,8 @@ function power(base, exponent) {
     if (exponent === 0) return 1;
     return base * power(base, exponent - 1);
 }
+
+//Example 
+console.log(power(2, 3));
+console.log(power(5, 0));
+console.log(power(3, 4));
